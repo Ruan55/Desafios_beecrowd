c4d94489ed750ad2707ffc49cf548ca0aed194ce
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-07-17 11:24:57 -0300

    Add files via upload

---
+++ Desafios_beecrowd/1060.py
@@ -0,0 +1,13 @@
+numero = 0
+
+for i in range(6):
+
+    numeros = float(input())
+
+    if numeros > 0:
+        numero += 1
+    else:
+        numero == 0
+
+print(numero, "valores positivos")
+        
